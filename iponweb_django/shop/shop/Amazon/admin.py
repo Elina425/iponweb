9481747f7f74d6651bd36cc877126485eb6dab56
Elina425 <elinamelkonyan4@gmail.com>
2023-02-15 23:47:26 +0400

    homework8

--- iponweb_django/shop/shop/Amazon/admin.py
+++ iponweb_django/shop/shop/Amazon/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
 from .models import Customer
+from .models import Category
+from .models import Item
+from .models import Store
+from .models import StoreOwner
+from .models import Mybug
+from .models import Purchase
 # Register your models here.
-class BuyerAdmin(admin.ModelAdmin):
+class CustomerAdmin(admin.ModelAdmin):
     list_display = ("id", "registrated_at", "user_info")
     def user_info(self, obj):
         if obj.user:
             return "{}".format(" ". join(list(obj.user.first_name, obj.user.last_name)))
-admin.site.register(Customer, BuyerAdmin)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'image']
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['category', 'name', 'created_by', 'image', 'price', 'quantity', 'info', 'store']
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner', 'store_category']
+@admin.register(StoreOwner)
+class StoreOwnerAdmin(admin.ModelAdmin):
+    horizontal_filter = ['name', 'avatar', 'registered_at', 'slug']
+@admin.register(Mybug)
+class MybugAdmin(admin.ModelAdmin):
+    horizontal_filter = ['items', 'customer', 'total_price']
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    horizontal_filter = ['items', 'buy_time', 'customer', 'total_price']
+
+admin.site.register(Customer, CustomerAdmin)
--- iponweb_django/shop/shop/settings.py
+++ iponweb_django/shop/shop/settings.py
@@ -138,13 +138,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
-MEDIA_URL = "media/"
+STATIC_URL = '/static/'
+STATIC_DIR = Path().joinpath(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_DIR = Path().joinpath(BASE_DIR, "media")
+MEDIA_ROOT = MEDIA_DIR
